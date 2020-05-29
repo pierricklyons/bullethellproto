24b9a0ae03c3929b6ded63c98dc3aae79f6b4e9b
flyingmisaki <pierrick@live.co.uk>
2020-05-29 19:01:07 +0100

    Refactored code and added Entity, Enemy class

--- enemy.py
+++ enemy.py
@@ -4,5 +4,5 @@ from entity import Entity
 class Enemy(Entity):
 
     def __init__(self, game, spriteFilename, hitRadius, movementSpeed, x, y):
-        sprite = arcade.Sprite(Enemy.SPRITE_FILENAME, scale = 0.5)
+        sprite = arcade.Sprite(spriteFilename, scale = 0.5)
         super().__init__(game, sprite, Enemy.HIT_RADIUS, x, y)
